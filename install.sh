de2cb992c8206cd6224944d56c776e92d7afcf29
Xin Zhou <maiziex@gmail.com>
2018-11-02 14:24:21 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,13 @@ curl -L https://github.com/attractivechaos/k8/releases/download/v0.2.4/k8-0.2.4.
 # download the reference file (GRCh38)
 wget http://xinzhouneuroscience.org/wp-content/uploads/2018/10/GRCh38_reference.tar.gz
 tar -xvf GRCh38_reference.tar.gz
+
+conda install -c bioconda bcftools 
+
+
+
+
+echo 'You have installed AncetralCall and the dependencies successfully!'
  
 
 
