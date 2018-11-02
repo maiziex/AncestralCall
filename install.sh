4d1b46d1d49c3afaa2519d8fb58fbd5b1a6f6364
Xin Zhou <maiziex@gmail.com>
2018-11-02 15:10:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,10 @@ curl -L https://github.com/attractivechaos/k8/releases/download/v0.2.4/k8-0.2.4.
 wget http://xinzhouneuroscience.org/wp-content/uploads/2018/10/GRCh38_reference.tar.gz
 tar -xvf GRCh38_reference.tar.gz
 
+conda install -c bioconda samtools 
 conda install -c bioconda bcftools 
+conda install -c bioconda htslib 
+
 
 
 
